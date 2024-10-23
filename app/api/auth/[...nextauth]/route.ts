8ee920c67726957a4e63466555fcaad6ce0fbbce
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-23 17:13:55 +0530

    Next Auth Secret added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -71,8 +71,8 @@ const handler = NextAuth({
     strategy: 'jwt',
   },
   jwt: {
-    secret:SECRET_KEY,
-    //secret: process.env.NEXTAUTH_SECRET, 
+    ///secret:SECRET_KEY,
+    secret: process.env.NEXTAUTH_SECRET, 
   },
   callbacks: {
     async jwt({ token, user }) {
