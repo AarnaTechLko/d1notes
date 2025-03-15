b7044796a5f5bedca87adc2a7e9d6b27931cb2f0
Harsh Bodgal <harshbodgal22@gmail.com>
2025-03-15 13:32:19 -0700

    allowed user to upload word/jpeg/png

--- app/coach/profile/page.tsx
+++ app/coach/profile/page.tsx
@@ -737,7 +737,7 @@ const Profile: React.FC = () => {
                     placeholder=' '
                     type="file"
                     name="youtube"
-                    accept="image/*,application/pdf"
+                    accept=".doc,.docx,application/msword,application/pdf,image/jpeg,image/png,.jpg"
                     className="border border-gray-300 rounded-lg py-2 px-4 w-full"
                     onChange={handleCVChange}
 
