44b4f78dbe01977b921acb377735344da70bd4c7
Harsh Bodgal <harshbodgal22@gmail.com>
2025-03-15 13:56:59 -0700

    generalized formats

--- app/coach/profile/page.tsx
+++ app/coach/profile/page.tsx
@@ -737,7 +737,7 @@ const Profile: React.FC = () => {
                     placeholder=' '
                     type="file"
                     name="youtube"
-                    accept=".doc,.docx,application/msword,application/pdf,image/jpeg,image/png,.jpg"
+                    accept=".doc,.docx,application/msword,application/pdf,image/*,"
                     className="border border-gray-300 rounded-lg py-2 px-4 w-full"
                     onChange={handleCVChange}
 
