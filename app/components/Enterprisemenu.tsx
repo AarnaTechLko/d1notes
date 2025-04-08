4237b19ca546915643fd72c3fa86339ce83edad1
Wizardzaron <ryan013192@hotmail.com>
2025-04-07 22:44:36 -0700

    made changes to logout, player profile, and password visibility for login

--- app/components/Enterprisemenu.tsx
+++ app/components/Enterprisemenu.tsx
@@ -7,7 +7,7 @@ interface EnterpriseMenuProps {
     session: any; // Adjust to your actual session type
     closeMenu: () => void;
     isActiveLink: (path: string) => string;
-    handleLogout: () => void;
+    handleLogout: (event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void;
     toggleHelp:()=>void;
     toggleDropdown:()=>void;
     toggleCreateAccount:()=>void;
